f341b189b7d5e629d9f1651e5a3fa5eda832dfe2
Anggie Viranata <anggieviranata22@gmail.com>
2025-11-04 20:13:43 +0700

    Commit 1 - Setup Awal

---
+++ penjualan.php
@@ -0,0 +1,9 @@
+<?php
+// --POLGAN MART--
+// Setup awal
+$kode_barang = ["B001", "B002", "B003", "B004", "B005"];
+$nama_barang = ["Teh Pucuk", "roti", "Sprite", "Chitato", "Indomie"];
+$harga_barang = [5000, 5000, 4000, 8000, 3500];
+
+echo "<h2> --POLGAN MART-- </h2>";
+?>
